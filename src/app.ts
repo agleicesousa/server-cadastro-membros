bfd9ada03f2a755fceeca420dd24cfec1a105a6f
agleicesousa <agleicesousa98@gmail.com>
2025-01-03 06:31:42 -0300

    refactor: database connection to use PostgresDataSource and remove connection.config

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,21 @@
 import express from 'express';
 import cors from 'cors';
-// import { PostgresDataSource } from './config/database.config';
-import connectionString from './config/connection.config';
+import { PostgresDataSource } from './config/database.config';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerDocument } from './config/swagger.config';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.SERVER_PORT
+const port = process.env.SERVER_PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// PostgresDataSource.initialize()
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log(`Server running at http://localhost:${port}`);
-//     });
-//   })
-//   .catch((error) => console.log(error));
-
-  connectionString.connect()
+PostgresDataSource.initialize()
   .then(() => {
     app.listen(port, () => {
-      console.log(`Conected, Server running at http://localhost:${port}`);
+      console.log(`Server running at http://localhost:${port}`);
     });
   })
   .catch((error) => console.log(error));
--- src/config/connection.config.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { Pool } from 'pg';
-
-const connectionString = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-});
-
-export default connectionString;
