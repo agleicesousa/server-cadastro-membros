77a9d2fa5e29ee4a9c08f481dddc6a333b298e07
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-02-05 07:16:38 -0300

    Refator: SEED code to use generic fake data generation function.

--- src/seeds/database.seed.ts
+++ src/seeds/database.seed.ts
@@ -11,103 +11,77 @@ import {
 import { faker } from '@faker-js/faker';
 
 async function initializeDatabase() {
-  if (!PostgresDataSource.isInitialized) {
-    await PostgresDataSource.initialize();
-    console.log('Banco de dados inicializado.');
-  }
+  if (!PostgresDataSource.isInitialized) await PostgresDataSource.initialize();
 }
 
-function generateFakeUsers(count: number, defaults = {}) {
-  return Array.from({ length: count }).map(() => ({
-    accountType: faker.helpers.arrayElement(Object.values(accountType)),
-    ageRange: faker.helpers.arrayElement(Object.values(ageRange)),
-    registrationNumber: faker.string.numeric(8),
-    fullName: faker.person.fullName(),
-    gender: faker.helpers.arrayElement(Object.values(gender)),
-    cpf: faker.string.numeric(11),
-    birthDate: faker.date.birthdate({ min: 18, max: 60, mode: 'age' }),
-    email: faker.internet.email(),
-    phone: `+55 (${faker.string.numeric(2)}) ${faker.string.numeric(5)}-${faker.string.numeric(4)}`,
-    password: faker.string.alphanumeric(10),
-    ...defaults
-  }));
-}
-
-function generateFakeAddresses(count: number) {
-  return Array.from({ length: count }).map(() => ({
-    country: 'Brazil',
-    stateInitials: faker.helpers.arrayElement(Object.values(stateInitials)),
-    city: faker.location.city(),
-    neighborhood: faker.location.streetAddress(),
-    street: faker.location.street(),
-    number: parseInt(faker.string.numeric(3)),
-    complement: faker.lorem.words(2),
-    cep: faker.string.numeric(8)
-  }));
-}
-
-function generateFakeDepartments(count: number) {
-  return Array.from({ length: count }).map(() => ({
-    name: faker.company.name()
-  }));
-}
+const generateFakeData = (
+  count: number,
+  defaults: object = {},
+  type: 'users' | 'addresses' | 'departments'
+) => {
+  return Array.from({ length: count }).map(() => {
+    switch (type) {
+      case 'users':
+        return {
+          accountType: faker.helpers.arrayElement(Object.values(accountType)),
+          ageRange: faker.helpers.arrayElement(Object.values(ageRange)),
+          registrationNumber: faker.string.numeric(8),
+          fullName: faker.person.fullName(),
+          gender: faker.helpers.arrayElement(Object.values(gender)),
+          cpf: faker.string.numeric(11),
+          birthDate: faker.date.birthdate({ min: 18, max: 60, mode: 'age' }),
+          email: faker.internet.email(),
+          phone: `+55 (${faker.string.numeric(2)}) 9${faker.string.numeric(4)}-${faker.string.numeric(4)}`,
+          password: faker.string.alphanumeric(10),
+          ...defaults
+        };
+      case 'addresses':
+        return {
+          country: 'Brazil',
+          stateInitials: faker.helpers.arrayElement(
+            Object.values(stateInitials)
+          ),
+          city: faker.location.city(),
+          neighborhood: faker.location.streetAddress(),
+          street: faker.location.street(),
+          number: parseInt(faker.string.numeric(3)),
+          complement: faker.lorem.words(2),
+          cep: faker.string.numeric(8)
+        };
+      case 'departments':
+        return { name: faker.company.name() };
+      default:
+        return {};
+    }
+  });
+};
 
 async function seedDatabase() {
   try {
     await initializeDatabase();
 
-    const memberRepository = PostgresDataSource.getRepository(Member);
-    const addressRepository = PostgresDataSource.getRepository(Address);
-    const departmentRepository = PostgresDataSource.getRepository(Department);
+    const memberRepo = PostgresDataSource.getRepository(Member);
+    const addressRepo = PostgresDataSource.getRepository(Address);
+    const departmentRepo = PostgresDataSource.getRepository(Department);
 
-    // Criar endereços
-    console.log('Criando endereços fakes...');
-    const fakeAddresses = generateFakeAddresses(10);
-    const newAddresses = addressRepository.create(fakeAddresses);
-    const savedAddresses = await addressRepository.save(newAddresses);
-    console.log('Endereços fakes criados com sucesso!');
-    if (!savedAddresses || savedAddresses.length === 0) {
-      throw new Error('Nenhum endereço foi salvo no banco de dados.');
-    }
-
-    // Criar departamentos fakes
-    console.log('Criando departamentos fakes...');
-    const fakeDepartments = generateFakeDepartments(5);
-    const newDepartments = departmentRepository.create(fakeDepartments);
-    const savedDepartments = await departmentRepository.save(newDepartments);
-    console.log('Departamentos fakes criados com sucesso!');
-    if (!savedDepartments || savedDepartments.length === 0) {
-      throw new Error('Nenhum departamento foi salvo no banco de dados.');
-    }
-
-    // Criar membros e associar endereços e departamentos
-    console.log('Criando membros fakes com endereços e departamentos...');
-    const fakeMembers = generateFakeUsers(10).map((member) => {
-      const randomAddress = faker.helpers.arrayElement(savedAddresses);
-      const randomDepartments = faker.helpers
-        .shuffle(savedDepartments)
-        .slice(0, 2);
-      if (!randomAddress) {
-        console.error('Erro: Endereço não encontrado para o membro');
-      }
-      return {
-        ...member,
-        address: randomAddress,
-        departments: randomDepartments
-      };
-    });
-
-    const newMembers = memberRepository.create(fakeMembers);
-    await memberRepository.save(newMembers);
-
-    console.log(
-      'Membros fakes com endereços e departamentos criados com sucesso!'
+    const fakeAddresses = await addressRepo.save(
+      addressRepo.create(generateFakeData(10, {}, 'addresses'))
     );
+    const fakeDepartments = await departmentRepo.save(
+      departmentRepo.create(generateFakeData(5, {}, 'departments'))
+    );
+
+    const fakeMembers = generateFakeData(10, {}, 'users').map((member) => ({
+      ...member,
+      address: faker.helpers.arrayElement(fakeAddresses),
+      departments: faker.helpers.shuffle(fakeDepartments).slice(0, 2)
+    }));
+
+    await memberRepo.save(memberRepo.create(fakeMembers));
+
+    console.log('Banco de dados populado com sucesso!');
   } catch (error) {
-    console.error(
-      'Erro ao criar membros, endereços e departamentos fakes:',
-      error
-    );
+    console.error('Erro ao popular o banco de dados:', error);
   }
 }
 
